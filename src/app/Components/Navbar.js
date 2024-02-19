2ff63ba158af9400ee34990048f59e7794f20848
ukasha36 <ukashashabbir36@gmail.com>
2024-02-19 19:01:49 +0500

    about updated

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -9,11 +9,11 @@ import { Component, useState } from "react";
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
   return (
-    <div>
+    <div  className="  md:h-[6rem] min-w-full">
       <nav className="w-full bg-white  top-0 left-0 right-0 z-10 md:pt-2">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex items-center justify-between md:py-3  md:block">
               {/* LOGO */}
               <Link href="/">
               <div className=" md:w-full flex  ">
@@ -22,7 +22,7 @@ function NavBar() {
               width={130}
               height={90}
               alt="logo "
-              className=" sm:w-60 sm:h-28   md:w-60 md:h-16 object-none"
+              className=" sm:w-60 sm:h-28   md:w-60 md:h-16 object-contain"
             />
           </div>
               </Link>
@@ -52,39 +52,39 @@ function NavBar() {
               </div>
             </div>
           </div>
-          <div>
+          <div  className=" md:w-[50rem]">
             <div
               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
                 navbar ? "p-12 md:p-0 block" : "hidden"
               }`}
             >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex uppercase ">
-                <li className="pb-3  sm:text-xl md:text-lg font-sans md:text-brand-50 font-semibold sm:text-brand-200  py-2 md:px-6 text-center border-b-2 md:border-b-0    border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent navbar-link">
+              <ul className="h-screen md:h-auto items-center justify-center md:flex capitalize  font-monte ">
+                <li className="pb-3  sm:text-xl md:text-lg font-monte md:text-brand-50 font-semibold sm:text-brand-200  py-2 md:px-6 text-center border-b-2 md:border-b-0    border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent navbar-link">
                   <Link href="/" onClick={() => setNavbar(!navbar)}>
                 Home
                   </Link>
                   </li>
-                <li className="pb-3  sm:text-xl  md:text-lg font-sans md:text-brand-50 font-semibold  sm:text-brand-200 py-2 md:px-6 text-center border-b-2 md:border-b-0    navbar-link">
+                <li className="pb-3  sm:text-xl  md:text-lg font-monte md:text-brand-50 font-semibold  sm:text-brand-200 py-2 md:px-6 text-center border-b-2 md:border-b-0    navbar-link">
                   <Link href="/about" onClick={() => setNavbar(!navbar)}>
                     About us 
                   </Link>
                 </li>
-                <li className="pb-3 sm:text-xl md:text-lg md:text-brand-50 sm:text-brand-200 font-sans py-2 px-6 text-center font-semibold border-b-2 md:border-b-0    border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent navbar-link">
+                <li className="pb-3 sm:text-xl md:text-lg md:text-brand-50 sm:text-brand-200 font-monte py-2 px-6 text-center font-semibold border-b-2 md:border-b-0    border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent navbar-link">
                   <Link href="/service " onClick={() => setNavbar(!navbar)}>
                 Services
                   </Link>
                 </li>
-                <li className="pb-3  sm:text-xl  md:text-lg md:text-brand-50 sm:text-brand-200 font-sans py-2 px-6 text-center font-semibold border-b-2 md:border-b-0     border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent navbar-link">
+                <li className="pb-3  sm:text-xl  md:text-lg md:text-brand-50 sm:text-brand-200 font-monte py-2 px-6 text-center font-semibold border-b-2 md:border-b-0     border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent navbar-link">
                   <Link href="/contact" onClick={() => setNavbar(!navbar)}>
                     Contact
                   </Link>
                 </li>
-                <li className="pb-3 sm:text-xl  md:text-lg md:text-brand-50  sm:text-brand-200 font-sans py-2 px-6 text-center font-semibold border-b-2 md:border-b-0   navbar-link">
+                <li className="pb-3 sm:text-xl  md:text-lg md:text-brand-50  sm:text-brand-200 font-monte py-2 px-6 text-center font-semibold border-b-2 md:border-b-0   navbar-link">
                   <Link href="/packages" onClick={() => setNavbar(!navbar)}>
                     Packages
                     </Link>
                   </li>
-                <li className="sm:pb-2 md:pb-2 md:w-40 sm:text-xl md:text-base  py-2 px-2 text-center hover:sm:bg-brand-100  font-sans hover:text-brand-300  md:rounded-md    font-semibold md:text-brand-100   md:border md:border-solid md:border-brand-100 sm:text-brand-200   ">
+                <li className="sm:pb-2 md:pb-2 md:w-40 sm:text-xl md:text-base  py-2 px-2 text-center hover:sm:bg-brand-100  font-monte hover:text-brand-300  md:rounded-md    font-semibold md:text-brand-100   md:border md:border-solid md:border-brand-100 sm:text-brand-200   ">
                   <Link href="/touch" onClick={() => setNavbar(!navbar)}>
                   Get in Touch 
                   </Link>
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,4 +1,5 @@
 "use client";
+import  { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import { global } from "styled-jsx/css";
@@ -22,12 +23,17 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 export default function About() {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  // Handler function to update selected option
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
   return (
     <>
       <div className="bg-brand-100 flex sm:flex-col sm:items-center  sm:h-[43rem] md:flex-row md:h-screen min-w-full md:justify-evenly md:items-center">
         <div className="sm:h-3/5 sm:w-11/12 sm:justify-center md:h-3/5  md:w-2/5 pt-7 flex  flex-col text-pretty md:justify-evenly ">
           <h2 className=" sm:text-5xl sm:mb-[1.4rem]  md:mb-1 uppercase md:text-7xl font-monte font-extrabold   text-brand-500  sm:text-wrap md:text-clip md:leading-9  ">
-         
             About US
           </h2>
           <p className=" sm:text-[0.8rem] md:text-lg   leading-6 mb-4 text-brand-500 font-monte   sm:text-justify  ">
@@ -52,38 +58,345 @@ export default function About() {
 
       {/* Section 2 */}
 
-      <div className="sm:h-[43rem] md:h-[120vh] min-w-full flex md:justify-center  md:items-center">
+      <div className="  sm:h-[43rem] md:h-[120vh]  min-w-full flex md:justify-center  md:items-center">
         <div
-          className="bg-cover bg-left bg-no-repeat md:w-[60rem] md:h-4/5"
+          className=" sm:bg-contain sm:flex sm:justify-center sm:h-[70rem] md:block md:bg-cover bg-left bg-no-repeat md:w-[60rem] md:h-4/5"
           style={{
             backgroundImage: "url('/about6.jpg')",
-            backgroundSize: "cover", // Adjust background size
             backgroundPosition: "center", // Center the background image
           }}
         >
-          <div className=" md:absolute bg-brand-300 sm:h-3/5  sm:justify-center md:h-[30rem]  md:w-[26rem] pt-7 flex  flex-col text-pretty md:justify-evenly ">
+          <div className="  sm:w-[90%] sm:h-[26rem] sm:static  md:absolute bg-brand-300    sm:justify-evenly md:h-[30rem]  md:w-[26rem] pt-7 flex  flex-col text-pretty md:justify-evenly ">
+            <div className="flex sm:h-5  md:h-[5rem] w-[20rem] items-center ">
+              <Image
+                src="/line2.svg"
+                width={40}
+                height={10}
+                alt="logo "
+                className=" sm:w-[4rem] md:h-[4rem] md:w-[5rem]"
+              />
+              <h4 className=" font-monte text-[12px] font-bold  pl-4 ">
+                ABOUT US{" "}
+              </h4>
+            </div>
+            <h2 className=" sm:text-xl sm:mb-[1.4rem]  md:mb-1 uppercase md:text-3xl font-monte font-extrabold    text-brand-400  sm:text-wrap md:text-clip md:leading-9  ">
+              Craft Your Unique Web Creation
+            </h2>
+            <p className=" sm:text-[0.8rem] md:text-sm   leading-6 mb-4 text-brand-50 font-monte   sm:text-left  ">
+              Embrace the digital era and make your mark with a website that
+              goes beyond the ordinary. American Web Express offers innovative
+              tools and professional support to help you create a stunning
+              online presence that resonates with your audience and showcases
+              your individuality. Let us guide you on a creative journey to
+              develop a web masterpiece that stands out and leaves a lasting
+              impression in the online world.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* section 3  */}
+
+      <div className=" sm:h-[69rem]  sm:items-center  sm:flex-col md:h-[70vh] md:min-w-full md:flex-row flex justify-evenly ">
+        <div className=" sm:h-[22rem] sm:w-[90%] sm:justify-evenly   md:w-[25%] md:h-full flex flex-col">
           <div className="flex sm:h-5  md:h-[5rem] w-[20rem] items-center ">
             <Image
               src="/line2.svg"
               width={40}
               height={10}
               alt="logo "
-              className="h-[4rem] w-[5rem]"
+              className=" sm:w-[4rem] md:h-[4rem] md:w-[5rem]"
             />
-            <h4 className=" font-monte text-[12px] font-bold  pl-4 ">ABOUT US </h4>
+            <h4 className=" font-monte text-[13px] font-bold    ">
+              OUR VISION{" "}
+            </h4>
           </div>
-            <h2 className=" sm:text-5xl sm:mb-[1.4rem]  md:mb-1 uppercase md:text-3xl font-monte font-extrabold    text-brand-400  sm:text-wrap md:text-clip md:leading-9  ">
-              Craft Your Unique Web Creation
+          <h2 className=" sm:text-xl sm:mb-[1.4rem]  md:mb-1 uppercase md:text-[1.4rem] font-monte font-extrabold    text-brand-400  sm:text-wrap md:text-clip md:leading-9  ">
+            Follow the Digital Trends & Lead the Changes
+          </h2>
+          <p className=" sm:text-[0.8rem]  line mb-4 text-brand-50 font-monte   sm:text-left md:text-sm   ">
+            American Web Express to empower online businesses in a world
+            brimming with prosperity, connectivity, growth, and purpose. With
+            our Web and SEO expertise we are dedicated to delivering tangible
+            results for clients across diverse industry sectors.
+          </p>
+        </div>
+
+        <div className=" sm:h-[22rem] sm:w-[90%] sm:justify-evenly   md:w-[25%] md:h-full flex flex-col">
+          <div className="flex sm:h-5  md:h-[5rem] w-[20rem] items-center ">
+            <Image
+              src="/line2.svg"
+              width={40}
+              height={10}
+              alt="logo "
+              className=" sm:w-[4rem] md:h-[4rem] md:w-[5rem]"
+            />
+            <h4 className=" font-monte text-[13px] font-bold    ">
+              OUR MISSION{" "}
+            </h4>
+          </div>
+          <h2 className=" sm:text-xl sm:mb-[1.4rem]  md:mb-1 uppercase  md:text-[1.4rem] font-monte font-extrabold    text-brand-400  sm:text-wrap md:text-clip md:leading-9  ">
+            The Biggest World Brands Always Take Risks
+          </h2>
+          <p className=" sm:text-[0.8rem]  line mb-4 text-brand-50 font-monte   sm:text-left md:text-sm   ">
+            We aim to establish ourselves as the premier digital marketing
+            agency in the UK. Our commitment is to provide businesses with
+            unparalleled and distinctive strategies, enabling them to fully
+            unleash their digital capabilities.
+          </p>
+        </div>
+
+        <div className=" sm:h-[22rem] sm:w-[90%] sm:justify-evenly   md:w-[25%] md:h-full flex flex-col">
+          <div className="flex sm:h-5  md:h-[5rem] w-[20rem] items-center ">
+            <Image
+              src="/line2.svg"
+              width={40}
+              height={10}
+              alt="logo "
+              className=" sm:w-[4rem] md:h-[4rem] md:w-[5rem]"
+            />
+            <h4 className=" font-monte text-[13px] font-bold    ">
+              OUR VALUE{" "}
+            </h4>
+          </div>
+          <h2 className=" sm:text-xl sm:mb-[1.4rem]  md:mb-1 uppercase  md:text-[1.4rem] font-monte font-extrabold    text-brand-400  sm:text-wrap md:text-clip md:leading-9  ">
+            Lets Boost Your Business & Grow Together
+          </h2>
+          <p className=" sm:text-[0.8rem]  line mb-4 text-brand-50 font-monte   sm:text-left md:text-sm   ">
+            We are committed to propelling your business forward and fostering
+            growth together. Through our tailored digital marketing solutions,
+            we elevate your online presence, boost conversions, and unlock the
+            full potential of your business.
+          </p>
+        </div>
+      </div>
+
+      <div className=" sm:min-w-full sm:h-[6rem]  bg-brand-500  flex justify-center items-end">
+        <h2 className=" sm:text-5xl font-monte">Reviews</h2>
+      </div>
+
+      <Swiper
+        className=" sm:h-[35rem] min-w-full md:h-[60vh] bg-brand-500 md:pt-4 font-monte "
+        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+        slidesPerView={1}
+        breakpoints={{
+          640: {
+            slidesPerView: 2, // Show 2 slides on medium screens
+            spaceBetween: 20, // Adjust spacing on medium screens
+          },
+          1024: {
+            slidesPerView: 3, // Show 3 slides on large screens
+            spaceBetween: 15, // Adjust spacing on large screens
+          },
+        }}
+        direction="horizontal" // Set direction to vertical
+        loop={true}
+        autoplay={{
+          delay: 2200,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true, // Enable pagination dots
+        }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className=" sm:w-[22rem]   md:m-1 sm:h-[20rem] md:w-[20rem] md:h-[15rem]  bg-brand-300 md:ml-10 flex flex-col border-full ">
+            <div className="md:h-2/5 md:w-full flex justify-between border-bottom">
+              <div className=" sm:pl-3  flex flex-col justify-center md:w-32 md:items-center">
+                <h3 className="sm:text-xl md:text-base"> Evanna Lynch </h3>
+                <Image
+                  src="/reviews.png"
+                  width={100}
+                  height={80}
+                  alt="logo "
+                  className=" sm:h-[2.5rem] "
+                />
+              </div>
+              <div className=" flex h-full w-24">
+                <Image
+                  src="/client3.png"
+                  width={150}
+                  height={120}
+                  alt="logo "
+                  className=" sm:w-60 sm:h-32   md:h-24 md:w-32 object-none"
+                />
+              </div>
+            </div>
+            <div className="  sm:p-4  md:h-2/4 md:w-full">
+              <p className="sm:text-[1.3rem]  md:leading-tight  sm:p-3 sm:text-left  text-brand-50    md:pl-2 md:text-[1.2rem] font-medium font-monte  ">
+                Remarkably smooth working with them, and I absolutely adore
+                every design they created for me
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className=" sm:w-[22rem] sm:h-[20rem] md:w-[20rem] md:h-[15rem]  bg-brand-300 md:ml-10 flex flex-col border-full ">
+            <div className="md:h-2/5 md:w-full flex justify-between border-bottom">
+              <div className=" sm:pl-3 md:pl-2  flex flex-col justify-center md:w-32 md:items-center">
+                <h3 className="sm:text-xl md:text-base"> Cidalia </h3>
+                <Image
+                  src="/reviews.png"
+                  width={100}
+                  height={80}
+                  alt="logo "
+                  className=" sm:h-[2.5rem] "
+                />
+              </div>
+              <div className=" flex h-full w-24">
+                <Image
+                  src="/client3.png"
+                  width={150}
+                  height={120}
+                  alt="logo "
+                  className=" sm:w-60 sm:h-32   md:h-24 md:w-32 object-none"
+                />
+              </div>
+            </div>
+            <div className="  sm:p-4  md:h-2/4 md:w-full">
+              <p className=" sm:text-[1.3rem] md:leading-tight sm:p-3 sm:text-left  text-brand-50  md:text-left md:text-[1.1rem] font-medium md:pl-2 font-monte   ">
+                Working with them was incredibly smooth, and I absolutely adore
+                every design they created for me.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className=" sm:w-[22rem] sm:h-[20rem] md:w-[20rem] md:h-[15rem]  bg-brand-300 md:ml-10 flex flex-col border-full ">
+            <div className="md:h-2/5 md:w-full flex justify-between border-bottom">
+              <div className=" sm:pl-3 flex flex-col justify-center md:w-32 md:items-center">
+                <h3 className="sm:text-xl md:text-base"> Jason knight </h3>
+                <Image
+                  src="/reviews.png"
+                  width={100}
+                  height={80}
+                  alt="logo "
+                  className=" sm:h-[2.5rem] "
+                />
+              </div>
+              <div className=" flex h-full w-24">
+                <Image
+                  src="/client3.png"
+                  width={150}
+                  height={120}
+                  alt="logo "
+                  className=" sm:w-60 sm:h-32   md:h-24 md:w-32 object-none"
+                />
+              </div>
+            </div>
+            <div className="  sm:p-4  md:h-2/4 md:w-full">
+              <p className=" sm:text-[1.3rem]  md:text-lg md:leading-tight sm:p-3 sm:text-left  md:text-[1.2rem] font-monte font-medium text-brand-50  md:text-left md:pl-2    ">
+                Working with Chris at American Web Express was worthwhile. They
+                were eager to please and ultimately delivered as promised.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className=" sm:w-[22rem] sm:h-[23rem] md:w-[20rem] md:h-[19rem]  bg-brand-300 md:ml-8 flex flex-col border-full ">
+            <div className="md:h-2/5 md:w-full flex justify-between border-bottom">
+              <div className=" sm:pl-3 flex flex-col justify-center md:w-32 md:items-center">
+                <h3 className="sm:text-xl md:text-base"> Robert Brown</h3>
+                <Image
+                  src="/reviews.png"
+                  width={100}
+                  height={80}
+                  alt="logo "
+                  className=" sm:h-[2.5rem] "
+                />
+              </div>
+              <div className=" flex h-full w-24">
+                <Image
+                  src="/client3.png"
+                  width={150}
+                  height={120}
+                  alt="logo "
+                  className=" sm:w-60 sm:h-32   md:h-24 md:w-32 object-none"
+                />
+              </div>
+            </div>
+            <div className="  sm:p-3  md:h-2/4 md:w-full">
+              <p className=" sm:text-[1.1rem] md:text-[1.1rem] font-medium md:leading-tight sm:p-3 sm:text-left  text-brand-50 md:p-1 md:text-left md:pl-2 font-monte   ">
+                American Web Express are excellent. Quick and efficient with
+                nothing being too much trouble. Would recommend to anyone
+                looking for SEO, web dev, Gfx , ppc.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+      {/* Form  */}
+
+      <div className=" sm:h-[80rem]  bg-brand-100  flex justify-center items-center md:h-[150vh] min-w-full">
+        <div className=" sm:h-[60rem]  sm:w-[90%]  md:h-[90%] md:w-[80%] bg-brand-500 flex flex-col sm:justify-evenly md:items-center">
+          <div className=" sm:h-[20rem] sm:w-full sm:pt-[1rem]  md:h-[16rem] md:w-[90%] flex flex-col  items-center justify-center" >
+            <h2 className=" sm:text-[1.9rem] sm:mb-[0.4rem] sm:text-center  md:mb-8 uppercase  md:text-[45px] font-monte font-extrabold    text-brand-400   ">
+              Get in Touch Today
             </h2>
-            <p className=" sm:text-[0.8rem] md:text-sm   leading-6 mb-4 text-brand-50 font-monte   sm:text-left  ">
-              Embrace the digital era and make your mark with a website that
-              goes beyond the ordinary. American Web Express offers innovative tools and
-              professional support to help you create a stunning online presence
-              that resonates with your audience and showcases your
-              individuality. Let us guide you on a creative journey to develop a
-              web masterpiece that stands out and leaves a lasting impression in
-              the online world.
+            <p className=" sm:text-[0.9rem] sm:text-center  sm:p-[0.7rem] md:text-sm text-brand-50 font-monte md:text-[16px]   ">
+              Start your online journey with American Web Express, offering
+              top-notch services including website development, web design, SEO,
+              graphics, and more. Gain a competitive edge at affordable rates.
             </p>
+          </div>
+
+          <div className=' sm:h-[58rem] sm:w-full md:w-full md:h-[40rem] flex flex-col '>
+              <div className=' sm:h-[10rem] sm:w-full sm:flex-col md:w-full md:h-[5rem] justify-evenly items-center flex md:flex-row ' > 
+              <input
+                type="text"
+                name="text"
+                id="text"
+                required
+                placeholder='Full Name'
+                autoComplete="off"
+                className=' sm:w-[18rem] sm:h-[3rem]   md:w-[21rem] md:h-[3rem] md:pl-4 '              />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                autoComplete="off"
+                placeholder=' Email '
+                className=' sm:w-[18rem] sm:h-[3rem]   md:w-[21rem] md:h-[3rem] md:pl-4 '    
+                          />
+              </div>
+              <div className=' sm:h-[10rem] sm:w-full sm:flex-col md:w-full md:h-[5rem] justify-evenly items-center flex md:flex-row ' > 
+
+              <input
+                type="text"
+                name="number"
+                id="number"
+                placeholder='Phone Number'
+                required
+                autoComplete="off"
+                className=' sm:w-[18rem] sm:h-[3rem]   md:w-[21rem] md:h-[3rem] md:pl-4 '    
+              />
+             <select value={selectedOption} onChange={handleSelectChange}     className=' sm:w-[18rem] sm:h-[3rem]   md:w-[21rem] md:h-[3rem] md:pl-4 '    >
+        <option value="">Web Development</option>
+        <option value="option1"> Graphic Design</option>
+        <option value="option2">Seo & Marketing</option>
+        <option value="option3"> Social Media Marketing</option>
+        {/* Add more options as needed */}
+      </select>
+              </div>
+              <div className=' sm:h-[20rem] sm:justify-evenly sm:items-center  md:h-[26rem] md:w-full flex flex-col  md:justify-around  md:items-center'> 
+              <textarea
+                name="message"
+                id="message"
+                cols="20"
+                rows="7"
+                placeholder=' Project Details'
+                required
+                 className=' sm:w-[18rem] md:w-[46rem] md:h-[20rem] pl-4 pt-4'
+              ></textarea>
+              <button type='submit' className="sm:pb-2 md:pb-2 md:w-40 sm:text-[12px]  sm:w-[9rem] md:text-base   py-2 px-2 text-center uppercase   font-monte     font-semibold    border md:border-solid border-brand-700    " > Contact US </button>
+              </div>
           </div>
         </div>
       </div>
